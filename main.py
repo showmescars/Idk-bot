ba5b41936d6b970730329e3169b9c6c4f7321bc0
showmescars <antoniobelljr100@gmail.com>
2026-02-14 13:19:57 -0800

    Remove unnecessary intents from bot setup

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.guilds = True
-intents.members = True
 bot = commands.Bot(command_prefix=’!’, intents=intents, help_command=None)
 
 # Files
